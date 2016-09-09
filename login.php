102f50f11cc557b39f5353f6a92331c56a9fbab7
Sergey <unanimity@list.ru>
2016-09-09 15:08:29 +0300

    Рабочая авторизация

--- login.php
+++ login.php
@@ -5,32 +5,36 @@ $user = "b7625632439068";
 $pwd = "1b4fd4f4";
 $db = "pj";
 
+$username = $_POST['username'];
+$pass= $_POST['password'];
 
 function login($arg_1, $arg_2)
-{  echo "function ok"; 
+{  
+
+  //echo "functional ok"; 
   echo 	"$arg_1";
-  
-$sql_select = "SELECT * FROM registration_tbl";
+  echo 	"$arg_2";
+$sql_select = mysql_query("SELECT * FROM PJ.USERS");
+if($sql_select) {
+ 
+echo 	" 1 ";} else echo 	" 9 ";
+
+
+
+
 $stmt = $conn->query($sql_select);
+  echo 	"$ 2 ";
 $registrants = $stmt->fetchAll(); 
-
- echo "<table>";
-
+  echo 	" 3 ";
 if(count($registrants) > 0) {
     echo "<h2>People who are registered:</h2>";
-    echo "<table>";
-    echo "<tr><th>Name</th>";
-    echo "<th>Email</th>";
-    echo "<th>Date</th></tr>";
-    foreach($registrants as $registrant) {
-        echo "<tr><td>".$registrant['name']."</td>";
-        echo "<td>".$registrant['email']."</td>";
-        echo "<td>".$registrant['date']."</td></tr>";
-    }
+    
     echo "</table>";
 } else {
     echo "<h3>No one is currently registered.</h3>";
 }
+
+
 }
 
 $func = 'login';
@@ -42,13 +46,18 @@ $func = 'login';
 try {
     $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	 echo "Connected successfully"; 
+	 echo "Connected successfully"; 	 
+	 $sql_select = "select * from users where LOGIN_USERS = '$username' and PASSWORD_USERS = '$pass'";
 	 
-	// header ('Location: QSO.html');  // перенаправление на нужную страницу
-     
-	 echo $_POST['username']; 
-	 
-	//$func($_POST['username'],$_POST['password']); 
+     $stmt = $conn->query($sql_select);
+	 if ($stmt){
+		 
+     $registrants = $stmt->fetchAll(); 
+	if(count($registrants) > 0)
+		{ 
+	header ('Location: QSO.html'); 
+	} 
+	 else 	header ('Location: index.html'); }
 	
 }
 catch(Exception $e){
