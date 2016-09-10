b4d0449dbd269b3dc6778d28cbddd115e3b7d4e6
Sergey <unanimity@list.ru>
2016-09-10 03:49:43 +0300

    передаю андрюще писать вставку в бд

--- QSO.php
+++ QSO.php
@@ -132,24 +132,24 @@ $result = last_N(242,3);
 		 <table class="tQSO" align="center">
 	<?php
 $myrow = mysql_fetch_array($result);
-printf('<tr><td>Band</td>
-			<td>Mode</td>
-			<td>Time</td>
-			<td>QsS</td>
-			<td>Call</td>
-			<td>RSTout</td>
-			<td>RSTin</td>
-			<td>NRin</td><tr>');
+printf('<tr><td><h4>Band</h4></td>
+			<td><h4>Mode</h4></td>
+			<td><h4>Time</h4></td>
+			<td><h4>QsS</h4></td>
+			<td><h4>Call</h4></td>
+			<td><h4>RSTout</h4></td>
+			<td><h4>RSTin</h4></td>
+			<td><h4>NRin</h4></td><tr>');
 do{
 	printf('<tr>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
-			<td>%s</td>
+		<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
+			<td><h4>%s</h4></td>
 			</tr>', $myrow['BAND_BANDS'],$myrow['NAME_MODS'],$myrow['dateq'],$myrow['NROUT_QSOS'],
 					$myrow['call_in'],$myrow['RST_OUT_QSOS'],$myrow['RSTIN_QSOS'],$myrow['NRIN_QSOS']);
 } while ($myrow = mysql_fetch_array($result));			
